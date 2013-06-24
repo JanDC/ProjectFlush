4a87334ec320d2af6e57c43428d49a11f3a1ce71
Tom Van Looy <tom@ctors.net>
2013-06-24 22:53:11 +0200

    remote DB

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 // Connecting, selecting database
-$link = mysql_connect('localhost', 'root', 'pi')
+$link = mysql_connect('192.168.1.14', 'pi', 'raspberry')
 or die('Could not connect: ' . mysql_error());
 mysql_select_db('project_flush') or die('Could not select database');
 ?>
